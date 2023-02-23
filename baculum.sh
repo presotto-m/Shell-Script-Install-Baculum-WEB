d10a43dc66d70a92a29e454cc99febef6219c36f
Matheus Presotto <83012707+presotto-m@users.noreply.github.com>
2023-02-23 06:27:35 -0800

    Update baculum.sh

--- baculum.sh
+++ baculum.sh
@@ -11,10 +11,10 @@ echo "deb http://www.bacula.org/downloads/baculum/stable-11/debian bullseye main
 deb-src http://www.bacula.org/downloads/baculum/stable-11/debian bullseye main" > /etc/apt/sources.list.d/baculum.list
 
 # Atualizando repositorio
-apt update
+apt update -y
 
 # Instalando baculum api
-apt-get install baculum-common baculum-api baculum-api-apache2
+apt-get install baculum-common baculum-api baculum-api-apache2 -y
 
 # Ativando modulo rewrite
 a2enmod rewrite
@@ -26,7 +26,7 @@ a2ensite baculum-api
 systemctl restart apache2
 
 #Instalando baculum web
-apt-get install baculum-common baculum-web baculum-web-apache2
+apt-get install baculum-common baculum-web baculum-web-apache2 -y
 
 # Ativando mudulo rewrite
 a2enmod rewrite
